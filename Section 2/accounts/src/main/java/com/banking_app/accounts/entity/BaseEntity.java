b35d6f008e45ed3ddf9a1064c5ee66d0326164d3
Abhay Sharma <avay.abhay@gmail.com>
2025-04-29 00:29:47 +0530

    creating baseEntity, adding h2 Db

---
+++ Section 2/accounts/src/main/java/com/banking_app/accounts/entity/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.banking_app.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter @ToString
+public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+}
